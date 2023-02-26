d1e463e783b6d812ed90996176bf75d200813976
SergejsRom <sergejsromanovskis@gmail.com>
2023-02-26 22:53:12 +0200

    login api

--- project/routes/api.php
+++ project/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\V1\Auth\RegisterController as RegisterController;
+use App\Http\Controllers\Api\V1\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('auth/register', RegisterController::class);
+Route::post('auth/register', Auth\RegisterController::class);
+Route::post('auth/login', Auth\LoginController::class);
