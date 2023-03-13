de9072c89be26a27aab13f8b0f1a6c0f2292f5a1
SergejsRom <sergejsromanovskis@gmail.com>
2023-03-13 16:45:54 +0200

    price

--- project/app/Http/Controllers/Api/V1/ParkingController.php
+++ project/app/Http/Controllers/Api/V1/ParkingController.php
@@ -45,6 +45,7 @@ class ParkingController extends Controller
 {
     $parking->update([
         'stop_time' => now(),
+        'total_price' => ParkingPriceService::calculatePrice($parking->zone_id, $parking->start_time),
     ]);
  
     return ParkingResource::make($parking);
